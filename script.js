8fb81c439d7859d0d4880b152c0efe7e223988ef
BiancaBarcelos <biancassbarcelos@gmail.com>
2023-08-03 17:58:56 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,8 @@
+$('.conteudo-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  });
